2201774715828e1c56c7d5586e1a2ce1506ce76a
NIRMITHA <avishkanirmitha@gmail.com>
2022-02-13 11:08:34 +0530

    done footer

--- src/Components/Footer/FooterComp.js
+++ src/Components/Footer/FooterComp.js
@@ -1,28 +1,30 @@
 import styled from 'styled-components';
 import FooterCol from './FooterCol';
+import FirstCol from './FirstCol';
 
 
 
 function FooterComp({className=""}) {
   return (
     <DIV className={`${className}`}>
-         <div className="FooterComp-wrapper">
+         <div className="FooterComp-wrapper pt-5 pb-5">
 
-                 <div className="row ">
-                         <div className="col-4 background-aqua">
-                                  <FooterCol title={"More from DirectDeal"} list={["Sell Fast","Doorstep Delivery","Membership","Banner Ads","Ad Promotions","Staffing solutions"]}></FooterCol>
+                 <div className="row gy-5">
+
+                         <div className="col-lg-4  col-sm-6  background-aqu">
+                                 <FirstCol></FirstCol>
                          </div>
 
-                         <div className="col-2 background-aqu">
+                         <div className="col-lg-2  col-sm-6  background-aqu">
                                   <FooterCol title={"More from DirectDeal"} list={["Sell Fast","Doorstep Delivery","Membership","Banner Ads","Ad Promotions","Staffing solutions"]}></FooterCol>
                          </div>
-                         <div className="col-2 background-aqu">
+                         <div className="col-lg-2  col-sm-6  background-aqu">
                                   <FooterCol title={"More from DirectDeal"} list={["Sell Fast","Doorstep Delivery","Membership","Banner Ads","Ad Promotions","Staffing solutions"]}></FooterCol>
                          </div>
-                         <div className="col-2 background-aqu">
+                         <div className="col-lg-2  col-sm-6  background-aqu">
                                   <FooterCol title={"More from DirectDeal"} list={["Sell Fast","Doorstep Delivery","Membership","Banner Ads","Ad Promotions","Staffing solutions"]}></FooterCol>
                          </div>
-                         <div className="col-2 background-aqu">
+                         <div className="col-lg-2  col-sm-6  background-aqu">
                                   <FooterCol title={"More from DirectDeal"} list={["Sell Fast","Doorstep Delivery","Membership","Banner Ads","Ad Promotions","Staffing solutions"]}></FooterCol>
                          </div>
                  </div>
